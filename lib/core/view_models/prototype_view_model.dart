2885ccf7071a9ee3832e2b4f8aaaf91a3d4094c8
Kevin Morelli <kev9318@gmail.com>
2021-03-03 23:57:51 -0300

    Checkbox prototype

--- lib/core/view_models/prototype_view_model.dart
+++ lib/core/view_models/prototype_view_model.dart
@@ -1,3 +1,30 @@
 import 'package:in_notes/core/view_models/base_view_model.dart';
 
-class PrototypeViewModel extends BaseViewModel {}
+class PrototypeViewModel extends BaseViewModel {
+  bool isChecked = false;
+  bool isCheckboxVisible = false;
+
+  void toggleCheckbox(bool value) {
+    isChecked = value;
+    notifyListeners();
+  }
+
+  String processTextChanged(String value) {
+    if (value.startsWith("​[]") && !isCheckboxVisible) {
+      isCheckboxVisible = true;
+      notifyListeners();
+
+      //! zero-width space below to detect the backspace with empty text, https://github.com/flutter/flutter/issues/14809
+      return value.replaceRange(0, 3, '​');
+    } else if (value.length == 0) {
+      isCheckboxVisible = false;
+      isChecked = false;
+      notifyListeners();
+
+      //! zero-width space below to detect the backspace with empty text, https://github.com/flutter/flutter/issues/14809
+      return '​';
+    }
+
+    return value;
+  }
+}
--- lib/ui/views/prototype.dart
+++ lib/ui/views/prototype.dart
@@ -10,13 +10,62 @@ class PrototypeView extends StatefulWidget {
 }
 
 class _PrototypeViewState extends State<PrototypeView> {
+  TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    //! zero-width space below to detect the backspace with empty text, https://github.com/flutter/flutter/issues/14809
+    _controller.text = '​';
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void processText(PrototypeViewModel viewModel) {
+    var processedValue = viewModel.processTextChanged(_controller.text);
+    if (processedValue != _controller.text) {
+      _controller.text = processedValue;
+      //! zero-width space below to detect the backspace with empty text, https://github.com/flutter/flutter/issues/14809
+      if (processedValue == '​') _controller.selection = TextSelection.collapsed(offset: _controller.text.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<PrototypeViewModel>.reactive(
       viewModelBuilder: () => PrototypeViewModel(),
       builder: (context, viewModel, child) => Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Visibility(
+                    visible: viewModel.isCheckboxVisible,
+                    child: Checkbox(
+                      value: viewModel.isChecked,
+                      onChanged: (value) => viewModel.toggleCheckbox(value),
+                    ),
+                  ),
+                  Expanded(
+                      child: TextField(
+                    style: TextStyle(
+                      decoration: viewModel.isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+                      color: viewModel.isChecked ? Colors.grey : Colors.black,
+                    ),
+                    controller: _controller,
+                    onChanged: (_) => processText(viewModel),
+                  )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
